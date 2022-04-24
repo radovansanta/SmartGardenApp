66855dd2516613147b2a05b2cbe31d463898cc19
RalGoX1 <293346@viauc.dk>
2022-04-24 19:10:16 +0200

    - Updated API url

--- app/src/main/java/com/example/smartgarden/Networking/Apis/ValveApi.java
+++ app/src/main/java/com/example/smartgarden/Networking/Apis/ValveApi.java
@@ -8,14 +8,17 @@ import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ValveApi {
     @GET("api/valves")
     Call<List<Valve>> getValve();
 
-    @PATCH("api/valves/{id}/{state}")
+    @POST("api/valves/{id}/{state}")
     Call<ResponseBody> updateValve(@Path("id") int id, @Path("state") String state);
+
 }
